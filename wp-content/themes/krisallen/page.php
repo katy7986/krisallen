33a82b5bd7054eab05f3640011a6122040214586
ingeniouslv <herb@ingeniouslv.com>
2014-04-18 17:00:24 -0700

    updated page template for tour&shows

--- wp-content/themes/krisallen/page.php
+++ wp-content/themes/krisallen/page.php
@@ -11,9 +11,32 @@
 					
 						<?php else : ?>
 							
+							<?php if( is_page('tour') ) : ?>
+	
+					<section id="post" class="container">
+					
+						<div class="row">
+							<article id="post-<?php the_ID(); ?>" class="span12 story" role="article" itemscope itemtype="http://schema.org/BlogPosting">	
+								<section class="post-text span8">
+									<h3 class="entry-title single-title"><?php _e("No tour days are available at this time", "bonestheme"); ?></h3>
+									<div class="divider"></div>
+									<section class="entry-content clearfix">
+										<p><?php _e("To tour dates are available at this time. Please check back soon for updates!", "bonestheme"); ?></p>
+									</section>
+									<footer class="article-footer">
+									    <p><?php _e("We will be announcing more dates soon!.", "bonestheme"); ?></p>
+									</footer>
+								</section>
+							</article>		
+						</div>
+							
+					</section>	
+					
+							<?php else : ?>
+							
 							<?php get_template_part( 'content', 'none' ); ?>
 					
-						<?php endif; ?>			
+						<?php endif; endif; ?>			
 				</div>
 					
 			</section>		
